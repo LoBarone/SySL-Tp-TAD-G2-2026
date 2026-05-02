96f30982a125945c8e9709b27bcc1a052d069d1b
Melina Barrientos <melinabarrientos0612@gmail.com>
2026-05-01 23:57:19 -0300

    Inicio breve del main o aplicacion

---
+++ Tp1/main.py
@@ -0,0 +1,23 @@
+from tad_cola import *
+from tad_trabajo import *
+from datetime import date,time
+
+#Como podria ser el menu si no consideramos con IF (queda muy redundante)
+
+opcion=int(input("Seleccione la opción que desea utilizar: "))
+
+match opcion:
+    case 1:
+        #procedimiento o codigo directamente
+    case 2:
+        #procedimiento o codigo directamente
+    case 3:
+        #procedimiento o codigo directamente
+    case 4:
+        #procedimiento o codigo directamente
+    case 5:
+        #procedimiento o codigo directamente
+    case 6:
+        #procedimiento o codigo directamente
+    case _:
+        print("Error: La opción que usted seleccionó es invalida. ")
