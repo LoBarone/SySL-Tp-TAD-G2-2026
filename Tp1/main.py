7d13f023bced030645f7031e480c25969c84901c
LoBarone <lorenzobar241@gmail.com>
2026-05-02 18:30:51 -0300

    boludeces

--- Tp1/main.py
+++ Tp1/main.py
@@ -1,23 +1,42 @@
 from tad_cola import *
 from tad_trabajo import *
-from datetime import date,time
 
-#Como podria ser el menu si no consideramos con IF (queda muy redundante)
 
-opcion=int(input("Seleccione la opción que desea utilizar: "))
+def main():
+    while True:
+        print("--------"*6 + "\nPrograma gestion de impresiones:\n" + "--------"*6)
+        print("\n [ 1 ] - Opcion 1\n [ 2 ] - Opcion 2\n [ 3 ] - Opcion 3\n [ 4 ] - Opcion 4\n [ 5 ] - Opcion 5\n [ 6 ] - Opcion 6\n [ 7 ] - Salir")
+        # Cambiar nombre opcion por el nombre correcto (me dio paja xd) -lolo
+        opcion = int(input("Seleccione la opción que desea utilizar:"))
 
-match opcion:
-    case 1:
-        #procedimiento o codigo directamente
-    case 2:
-        #procedimiento o codigo directamente
-    case 3:
-        #procedimiento o codigo directamente
-    case 4:
-        #procedimiento o codigo directamente
-    case 5:
-        #procedimiento o codigo directamente
-    case 6:
-        #procedimiento o codigo directamente
-    case _:
-        print("Error: La opción que usted seleccionó es invalida. ")
+        match opcion:
+            case 1:
+                #procedimiento o codigo directamente
+                pass
+            case 2:
+                #procedimiento o codigo directamente
+                pass
+            case 3:
+                #procedimiento o codigo directamente
+                pass
+            case 4:
+                #procedimiento o codigo directamente
+                pass
+            case 5:
+                #procedimiento o codigo directamente
+                pass
+            case 6:
+                #procedimiento o codigo directamente
+                pass
+            case 7:
+                print("Fin del programa!")
+                break
+            case _:
+                print("Error: La opción que usted seleccionó es invalida. ")
+
+def ejemplo(): # para cada solucion definamos una funcion que despues vamos a llamar dentro del main.
+    pass # el pass es solo para que no marque error, cuando escriban codigo saquenlo.
+
+
+if __name__ == "__main__":
+    main()
