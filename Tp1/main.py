71adab1dca6ce4d17e4adf7c963ae483975c096f
LoBarone <lorenzobar241@gmail.com>
2026-05-03 18:32:34 -0300

    cambie huevadas

--- Tp1/main.py
+++ Tp1/main.py
@@ -3,45 +3,39 @@ from tad_trabajo import *
 
 
 def main():
+    cola = crearCola()
     while True:
         print("--------"*6 + "\nPrograma gestion de impresiones:\n" + "--------"*6)
-        print("\n [ 1 ] - Opcion 1\n [ 2 ] - Opcion 2\n [ 3 ] - Opcion 3\n [ 4 ] - Opcion 4\n [ 5 ] - Opcion 5\n [ 6 ] - Opcion 6\n [ 7 ] - Salir")
+        print("\n [ 1 ] - Cargar Documento\n [ 2 ] - Opcion 2\n [ 3 ] - Opcion 3\n [ 4 ] - Opcion 4\n [ 5 ] - Opcion 5\n [ 6 ] - Opcion 6\n [ 7 ] - Salir")
         # Cambiar nombre opcion por el nombre correcto (me dio paja xd) -lolo
         
         opcion = int(input("Seleccione la opción que desea utilizar:"))
 
         match opcion:
             case 1:
-                #procedimiento o codigo directamente
-                c=crearCola()
-                RecepciónDeDocumentos(c)
-                pass
+                agregarTrabajo(cola)
             case 2:
-                #procedimiento o codigo directamente
                 pass
             case 3:
-                #procedimiento o codigo directamente
                 pass
             case 4:
-                #procedimiento o codigo directamente
                 pass
             case 5:
-                #procedimiento o codigo directamente
                 pass
             case 6:
-                #procedimiento o codigo directamente
                 pass
             case 7:
                 print("Fin del programa!")
                 break
             case _:
                 print("Error: La opción que usted seleccionó es invalida. ")
+                
 
-def RecepciónDeDocumentos(cola):
-    print("\n--- Carga de Nuevo Trabajo ---")
+def agregarTrabajo(cola):
+    print("--- Carga de Nuevo Trabajo ---")
     jobId = int(input("Ingrese el ID (0 para cancelar): "))
     
-    if jobId != 0:
+    if jobId > 0:
         nombDocu = input("Ingrese el Nombre: ")
         tipo = input("Ingrese el Tipo: ")
         cantPag = int(input("Ingrese la cantidad de Páginas: "))
@@ -54,8 +48,10 @@ def RecepciónDeDocumentos(cola):
         t = crearTrabajo()
         cargarTrabajo(t, jobId, nombDocu, tipo, cantPag, nivel, año, mes, dia, hora, minuto)
         encolar(cola, t)
-        print("Trabajo encoladoagregado con éxito.")
+        print("Trabajo agregado con éxito.")
     else:
-        print("Error.")
+        raise Exception("Error: Id ingresado no es valido")
+
+
 if __name__ == "__main__":
     main()
