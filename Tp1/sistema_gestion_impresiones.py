db44d1d477878d745b25ed1e5db90cc478c78955
Melina Barrientos <melinabarrientos0612@gmail.com>
2026-05-05 23:21:48 -0300

    Inciso 2 mas otras boludeces

--- Tp1/sistema_gestion_impresiones.py
+++ Tp1/sistema_gestion_impresiones.py
@@ -1,4 +1,4 @@
-from Tp1.tad_cola_trabajos import *
+from tad_cola_trabajos import *
 from tad_trabajo import *
 
 
@@ -15,7 +15,7 @@ def main():
             case 1:
                 agregarTrabajo(cola)
             case 2:
-                pass
+                cambioDePrioridad(cola)
             case 3:
                 pass
             case 4:
@@ -31,6 +31,7 @@ def main():
                 print("Error: La opción que usted seleccionó es invalida. ")
                 
 
+#Inciso 1
 def agregarTrabajo(cola):
     print("--- Carga de Nuevo Trabajo ---")
     jobId = int(input("Ingrese el ID (0 para cancelar): "))
@@ -50,7 +51,47 @@ def agregarTrabajo(cola):
         encolar(cola, t)
         print("Trabajo agregado con éxito.")
     else:
-        raise Exception("Error: Id ingresado no es valido")
+        print("Error: Id ingresado no es valido")
+
+
+#Inciso 2
+def cambioDePrioridad(cola):
+    #Averiguamos si la cola esta vacia (mo hay ningun elemento)
+    if colaVacia(cola):
+        print("La cola está vacía.")
+        return
+    
+    #Preparamos ciertos datos como el idBuscado, preparamos auxiliares.
+    idBuscado = int(input("Ingrese el ID de Trabaji que desea modificar: "))
+    idEncontrado = False
+    cola_auxiliar = crearCola()
+
+    #Mientras la cola no este vacia, recorremos cada posicion buscando el id de trabajo
+    while not colaVacia()
+        trabajo= desencolar(cola)
+
+        if verId(trabajo) == idBuscado :
+            nuevaPrioridad = input("Ingrese la nueva prioridad (n: Normal / e: Express): ")
+
+            if nuevaPrioridad == "n" or nuevaPrioridad == "e":
+                modPrioridad(trabajo, nuevaPrioridad)
+                print("Cambio exitoso! Prioridad Actualizada.")
+                idEncontrado= True
+            else:
+                print("Error: Prioridad no válida. No se realizaron cambios. ")
+
+        encolar(cola_auxiliar, trabajo)
+
+        if not idEncontrado:
+            print(f"No se encontró ningún trabajo con el ID {idBuscado}. ")
+
+    #Aca la cola original vuelve a ser guardada, ya con los cambios modificados correspondientes
+    while not colaVacia(cola_auxiliar):
+        encolar(cola, desencolar(cola_auxiliar))
+
+
+
+
 
 
 if __name__ == "__main__":
